91d3ba6dae3addaf519a150cbc8c7ba2ab19f1b2
alisaosx <aliasosx@hotmail.com>
2019-01-20 01:18:40 +0700

    update text

--- public/main.js
+++ public/main.js
@@ -2,25 +2,42 @@ const video = document.querySelector("#video");
 
 //var remoteControl = new RemoteControl(video);
 var recognition = new webkitSpeechRecognition();
+const Http = new XMLHttpRequest();
 recognition.continuous = true;
 recognition.interimResults = true;
 recognition.lang = "en-US";
 recognition.continuous = true;
 recognition.start();
-
+//responsiveVoice.speak("hello world");
 recognition.onresult = (event) => {
     for (let i = event.resultIndex; i < event.results.length; ++i) {
         if (event.results[i].isFinal) {
             console.log(event.results[i][0]);
             $("p").text(event.results[i][0].transcript.trim());
             if (event.results[i][0].transcript.trim() == "open") {
-                const Http = new XMLHttpRequest();
+
                 const url = 'https://localhost:8000/test';
                 Http.open("GET", url);
                 Http.send();
                 console.log('sent')
                 Http.onreadystatechange = (e) => {
-                    console.log(e)
+                    if (Http.responseText) {
+                        $("p").text("Server answer is " + Http.responseText);
+                        //responsiveVoice.speak("Server answer is " + Http.responseText);
+                    } else {
+                        $("p").text("Server is down cannot be proceed");
+                        //responsiveVoice.speak("Server is down cannot be proceed");
+                    }
+
+                }
+            } else if (event.results[i][0].transcript.trim() == "info") {
+                const url = 'http://localhost:8001/info';
+                Http.open("GET", url);
+                Http.send();
+                console.log('sent')
+                Http.onreadystatechange = (e) => {
+                    console.log(Http.responseText);
+                    $("p").text("Server answer is " + Http.responseText + ".");
                 }
             }
         }
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+
 const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 http.createServer(app).listen(8001, () => {
-    console.log('Server started on port ' + 8000);
+    console.log('Server started on port ' + 8001);
 });
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/info', (req, res) => {
+    var osvar = process.platform;
+    res.send(osvar);
 });
